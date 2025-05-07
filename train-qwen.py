348ccd29391406f459598e7bc8a265e04ffd1639
vallinayakichidambaram <acgvalli@gmail.com>
2025-05-07 19:37:40 +0530

    cuda

--- train-qwen.py
+++ train-qwen.py
@@ -1,16 +1,26 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling
 from datasets import load_dataset
+import torch
+
+torch.backends.cuda.enable_flash_sdp(False)
+torch.backends.cuda.enable_math_sdp(True)
+torch.backends.cuda.enable_mem_efficient_sdp(False)
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.is_available())
 
 dataset_path = "./dataset/solidity_data_instruct.txt"
 dataset = load_dataset("text", data_files={"train": dataset_path})
 
 model_name = "Qwen/Qwen2.5-Coder-1.5B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="cpu")
+model.config.use_sliding_window = False
+print(model.config)
 def tokenize_function(examples):
     return tokenizer(examples["text"], truncation=True, padding="max_length", max_length=512)
 
-tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns=["text"])
+tokenized_datasets = dataset.map(tokenize_function, batched=True, remove_columns=["text"], num_proc=1)
 
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 
@@ -22,8 +32,9 @@ training_args = TrainingArguments(
     save_total_limit=2,
     logging_steps=10,
     learning_rate=5e-5,
-    fp16=True,  
-    report_to="none"
+    fp16=False,  
+    report_to="none",
+    dataloader_pin_memory=False 
 )
 
 trainer = Trainer(
